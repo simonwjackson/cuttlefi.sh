5795b1d7c27d04da64aa62d0fc28fe1970a5806d
Simon W. Jackson <hello@simonwjackson.io>
2023-11-15 22:20:26 -0600

    :recycle: refactor: remove unused docopt_print_ARGS function
    - This function was previously used for debugging purposes.
    - It's no longer needed as the application is working as expected.
    - This change helps to keep the codebase clean and maintainable.

--- src/bin/podcast-cli-app.sh
+++ src/bin/podcast-cli-app.sh
@@ -115,26 +115,6 @@ list() {
   done
 }
 
-docopt_print_ARGS() {
-  local assoc="$1"
-  if [[ -z $assoc ]]; then
-    assoc=ARGS
-  fi
-
-  # bash dark magic copying $assoc argument to a local myassoc array
-  # inspired by:
-  # https://stackoverflow.com/questions/6660010/bash-how-to-assign-an-associative-array-to-another-variable-name-e-g-rename-t#8881121
-  declare -A myassoc
-  eval "$(typeset -A -p "$assoc" | sed "s/ $assoc=/ myassoc=/")"
-
-  # loop on keys
-  echo "docopt_print_ARGS => $assoc"
-  local a
-  for a in "${!myassoc[@]}"; do
-    printf "%20s = %s\n" "$a" "${myassoc[$a]}"
-  done
-}
-
 list=${ARGS[list]:-false}
 sync=${ARGS[sync]:-false}
 
