dd4fc613f596f1aeb4d7ac357c3adb212d490a30
Simon W. Jackson <hello@simonwjackson.io>
2023-11-16 21:42:14 -0600

    :twisted_rightwards_arrows: refactor(cli): change the command syntax
    - Change the syntax for the `sync` and `list` commands in `cuttlefi.sh`
    - The options are now specified before the command name
    - This change improves the usability of the CLI

--- src/bin/cuttlefi.sh
+++ src/bin/cuttlefi.sh
@@ -5,8 +5,8 @@ appName=$(basename "${0}")
 usage() {
   cat <<EOU
 Usage:
-  $appName sync [options]
-  $appName list [options]
+  $appName [options] sync
+  $appName [options] list
 
 Options:
   -c --config FILE      Specify an alternate configuration file.
@@ -35,58 +35,44 @@ root_dir="${ARGS[--root_dir]:-$(config_or_empty | yq --raw-output '.root_dir // 
 logs_dir="${ARGS[--logs_dir]:-$(config_or_empty | yq --raw-output '.logs_dir // "'"$(pwd)"'"')}"
 log_level="${ARGS[--log_level]:-$(config_or_empty | yq --raw-output '.log_level // "info"')}"
 
-# Function to download episodes for a given podcast
 download_podcast_episodes() {
   local podcast_name=$1
   local rss_feed_url=$2
   local save_dir="${root_dir}/${podcast_name}"
 
-  # Create directory for the podcast if it doesn't exist
   mkdir -p "$save_dir"
 
-  # Log file for the podcast
   local log_file="${logs_dir}/${podcast_name}.log"
 
-  # Process each item in the RSS feed
   wget -qO- "$rss_feed_url" |
     xmlstarlet sel -t -m "//item" -v "guid" \
       -o "|" -v "pubDate" \
       -o "|" -v "title" \
       -o "|" -v "enclosure/@url" -n |
     while IFS='|' read -r guid date title url; do
-      # Use GUID as unique identifier or hash the URL if GUID is missing
       local unique_id="${guid:-$(echo -n "$url" | md5sum | awk '{print $1}')}"
 
-      # Format the date
       formatted_date=$(date -d "$date" +%Y-%m-%d)
 
-      # Replace spaces with underscores in the title
       safe_title=$(echo "$title" | tr " " "_")
 
-      # Extract the file extension from the URL
       ext="${url##*.}"
 
-      # Construct the filename
       filename="${formatted_date}-${safe_title}-${unique_id}.${ext}"
 
-      # Check if the episode has already been downloaded
       if grep -q "$unique_id" "$log_file"; then
         [[ "$log_level" == "info" ]] && echo "Already downloaded: $filename"
         continue
       fi
 
-      # Download the episode
       [[ "$log_level" != "error" ]] && echo "Downloading episode: $url"
       wget -O "${save_dir}/${filename}" "$url"
 
-      # Log the downloaded episode
       echo "$unique_id" >>"$log_file"
     done
 }
 
-# Function to synchronize podcasts
 sync() {
-  # Create the directory to save episodes if it doesn't exist
   mkdir -p "$root_dir"
   mkdir -p "$logs_dir"
 
